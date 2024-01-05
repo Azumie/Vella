a700fb741523b99087d3d880b8c7424d54248b55
Azumie <azumie250@gmail.com>
2024-01-04 21:33:50 -0400

    Practicando

---
+++ Vella/Client/Pages/ListProducts.razor.cs
@@ -0,0 +1,51 @@
+﻿using Vella.Shared;
+
+namespace Vella.Client.Pages
+{
+    public partial class ListProducts
+    {
+        private List<Product> products;
+        private bool variable = true;
+        private string? producto;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(1000);
+            products = new List<Product>() {
+                new Product { Id = 1, Code = "P001", Name = "Producto 1", Description = "Descripción del Producto 1", UnitPrice = 19.99m, UnitAmount = null, Available = 50, Photo = "https://via.placeholder.com/150", Category = "Electrónicos"},
+                new Product
+                {
+                    Id = 2,
+                    Code = "P002",
+                    Name = "Producto 2",
+                    Description = "Descripción del Producto 2",
+                    UnitPrice = 29.99m,
+                    UnitAmount = 2.5m,
+                    Available = 25,
+                    Photo = "https://via.placeholder.com/150",
+                    Category = "Ropa"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Code = "P003",
+                    Name = "Producto 3",
+                    Description = "Descripción del Producto 3",
+                    UnitPrice = 9.99m,
+                    UnitAmount = 1.0m,
+                    Available = 100,
+                    Photo = "https://via.placeholder.com/150",
+                    Category = "Hogar"
+                },
+            };
+
+        }
+
+        private Task funcionMayus(string roducto)
+        {
+            producto = roducto.ToUpper();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Vella/Shared/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vella.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ?Code { get; set; }
+        public string Name { get; set; }
+        public string ?Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ?UnitAmount { get; set; }
+        public int Available { get; set; } = 0;
+        public string ?Photo { get; set; }
+        public string ?Category { get; set; }
+    }
+}
